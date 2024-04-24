4a99ea55816820258c1b9f9d6382dbe9fcb3841d
Marco VANNI <163298536+MVchara@users.noreply.github.com>
2024-04-24 18:52:43 +0200

    Update search.php

--- search.php
+++ search.php
@@ -7,38 +7,35 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-<div class="head">
-<img src="Rimberio.png"style="width:7%">
-<center>
-<nav>
-<div class="table">
-<ul>
-<li class="chat"><a href="chat.php">Ma Messagerie</a></li>
-<li class="acceuil"><a href="index.php">Se déconnecter</a></li>
-<li class="profil"><a href="profilbg.php">Mon Profil</a></li>
-</ul>
-</div>
-</nav>
-</center>
-</div>
-<div class="recherche">
-<h2>Résultats de la recherche</h2>
+		<div class="head">
+			<img src="Rimberio.png"style="width:7%">
+			<center>
+				<nav>
+					<div class="table">
+						<ul>
+							<li class="chat"><a href="chat.php">Ma Messagerie</a></li>
+							<li class="acceuil"><a href="index.php">Se déconnecter</a></li>
+							<li class="profil"><a href="profilbg.php">Mon Profil</a></li>
+						</ul>
+					</div>
+				</nav>
+			</center>
+		</div>
+		<div class="recherche">
+			<h2>Résultats de la recherche</h2>
 <?php 
 if(isset($_GET['s'])){
-$query=$_GET['s'];
-$stmt="SELECT * FROM inscription WHERE pseudonyme  LIKE '%$query%' ";
-$resultat=$conn->query($stmt);
-if($resultat->num_rows>0){
-while ($row = $resultat->fetch_assoc()){
-echo "<p>profil:".$row['pseudonyme']."</p>";
-}
-}else{
-echo "Aucun résultat trouvé";
-}
-$conn->close();
-}else{
-echo "Veuillez entrer un pseudonyme";
-}
+	$query=$_GET['s'];
+	$stmt="SELECT * FROM inscription WHERE pseudonyme  LIKE '%$query%' ";
+	$resultat=$conn->query($stmt);
+	if($resultat->num_rows>0){
+		while ($row = $resultat->fetch_assoc()){
+			echo "<p>profil:".$row['pseudonyme']."</p>";
+		}
+	}else{
+		echo "Aucun résultat trouvé";
+	}
+	$conn->close();
 ?>
-</body>
+	</body>
 </html>
