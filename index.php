9dbdb6c57a84b08fc5a35eeadd06984df6893fde
Marco VANNI <163298536+MVchara@users.noreply.github.com>
2024-04-24 18:50:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,72 +6,73 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-<div class="head">
-<img src="Rimberio.png"style="width:7%">
-<center>
-<nav>
-<div class="table">
-<ul>
-<li class="acceuil"><a href="index.php">Accueil</a></li>
-<li class="amicale"><a href="amicale.php">A propos</a></li>
-<li class="connexion"><a href="connexion.php">Se connecter</a></li>
-<li class="connexion"><a href="admin_conn.php">Espace admin</a></li>
-</ul>
-</div>
-</nav>
-</center>
-</div>
-<div style="padding: 50px 50px">
-<FONT FACE="Calibri">
-<div class="box">
-    <div class="figure">
-        <img src="young.jpg">
-		<div class="caption">
-			<div class="aboute">
-				<h2>Envie de rencontrer ton Twin Flame?</h2>
-				<p>Bienvenue sur Lhoub ! Le lieu idéal pour rencontrer de nouveaux amis et peut-être même trouver l'amour parmi une communauté de jeunes dynamiques. Rejoignez-nous pour créer des liens authentiques et partager des moments inoubliables. Let's connect!<br></br>
-<button class="Connect" type="submit" value="Connect"><a href="connexion.php">Connectez vous!</a></button>
-</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-<div class="photo">
-<font size=5 color=#34495E><h1> Notre Équipe</h1></font>
-<div class="equipe">
-<img src="arnaud.jpg" width=250 height=250>
-<div class="description">
-<p>Arnaud Regnier</p>
-</div>
-</div>
-<div class="equipe">
-<img src="imane.jpg" width=250 height=250>
-<div class="description">
-<p>Imane Safe</p>
-</div>
-</div>
-<div class="equipe">
-<img src="marco.jpeg" width=250 height=250>
-<div class="description">
-<p>Marco Vanni</p>
-</div>
-</div>
-</div>
-<footer>
-<div class="footer">
-<div class="inline">
-<ul>
-<li ><a href="index.php">Accueil</a></li>
-<li><a href="amicale.php#ami">Rencontres amicales</a></li>
-<li><a href="amicale.php#amour">Rencontres amoureuses</a></li>
-<li><a href="connexion.php">Se connecter</a></li>
-</ul>
-</div>
-<div class="bottom">
-<p>Copyright Site Web de Rencontre Lhoub 2024; Réalisé par: Imane Safe, Marco Vanni, Arnaud Regnier</p>
-</div>
-</div>
-</footer>
-</body>
+		<div class="head">
+			<img src="Rimberio.png"style="width:7%">
+			<center>
+				<nav>
+					<div class="table">
+						<ul>
+							<li class="acceuil"><a href="index.php">Accueil</a></li>
+							<li class="amicale"><a href="amicale.php">A propos</a></li>
+							<li class="connexion"><a href="connexion.php">Se connecter</a></li>
+							<li class="connexion"><a href="admin_conn.php">Espace admin</a></li>
+						</ul>
+					</div>
+				</nav>
+			</center>
+		</div>
+		<div style="padding: 50px 50px">
+			<FONT FACE="Calibri">
+				<div class="box">
+					<div class="figure">
+						<img src="young.jpg">
+						<div class="caption">
+							<div class="aboute">
+								<h2>Envie de rencontrer ton Twin Flame?</h2>
+								<p>Bienvenue sur Lhoub ! Le lieu idéal pour rencontrer de nouveaux amis et peut-être même trouver l'amour parmi une communauté de jeunes dynamiques. Rejoignez-nous pour créer des liens authentiques et partager des moments inoubliables. Let's connect!<br></br>
+								<button class="Connect" type="submit" value="Connect"><a href="connexion.php">Connectez vous!</a></button>
+							</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</FONT>
+		</div>
+		<div class="photo">
+			<font size=5 color=#34495E><h1> Notre Équipe</h1></font>
+			<div class="equipe">
+				<img src="arnaud.jpg" width=250 height=250>
+				<div class="description">
+					<p>Arnaud Regnier</p>
+					</div>
+				</div>
+			<div class="equipe">
+				<img src="imane.jpg" width=250 height=250>
+				<div class="description">
+					<p>Imane Safe</p>
+				</div>
+			</div>
+			<div class="equipe">
+				<img src="marco.jpeg" width=250 height=250>
+				<div class="description">
+					<p>Marco Vanni</p>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<div class="footer">
+				<div class="inline">
+					<ul>
+						<li ><a href="index.php">Accueil</a></li>
+						<li><a href="amicale.php#ami">Rencontres amicales</a></li>
+						<li><a href="amicale.php#amour">Rencontres amoureuses</a></li>
+						<li><a href="connexion.php">Se connecter</a></li>
+					</ul>
+				</div>
+				<div class="bottom">
+					<p>Copyright Site Web de Rencontre Lhoub 2024; Réalisé par: Imane Safe, Marco Vanni, Arnaud Regnier</p>
+				</div>
+			</div>
+		</footer>
+	</body>
 </html>
