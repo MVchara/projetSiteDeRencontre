e118778dc9102fbdd5a52e8c186f88ae8be8b92c
Marco VANNI <163298536+MVchara@users.noreply.github.com>
2024-04-24 18:59:18 +0200

    Update amicale.php

--- amicale.php
+++ amicale.php
@@ -5,108 +5,108 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="style.css">
 	</head>
-	<body>
-<div class="head">
-<img src="Rimberio.png"style="width:7%">
-<center>
-<nav>
-<div class="table">
-<ul>
-<li class="acceuil"><a href="index.php">Accueil</a></li>
-<li class="amicale"><a href="amicale.php">A propos</a></li>
-<li class="connexion"><a href="connexion.php">Se connecter</a></li>
-</ul>
-</div>
-</nav>
-</center>
-</div>
-<div class="marg">
-<font size="6" color="#FF9967"><p id="ami">Notre plateforme offre une manière simple et sécurisée de trouver des amis ou un partenaire à proximité ou partout dans le monde!</p></font>
-<h1>Rencontres Amicales:</h1>
-<font size="5"  ><p>Une fois que vous avez trouvé des profils qui vous intéressent, engagez la conversation en toute simplicité grâce à notre messagerie privée intégrée. Organisez des sorties, des activités ou simplement discutez en ligne, selon vos envies et disponibilités.</p>
-</font>
-<center>
-<div class="pic">
-<img src="night.jpg" width=350 height=200>
-<div class="ami">
-<p>Night Life</p>
-</div>
-</div>
-<div class="pic">
-<img src="socialnet.jpg"width=350 height=200>
-<div class="ami">
-<p>Social Networking</p>
-</div>
-</div>
-<div class="pic">
-<img src="dining.jpg" width=350 height=200>
-<div class="ami">
-<p>Dinner Entre Potes</p>
-</div>
-</div>
-<p>Et bien plus!!</p>
-</center>
-<h1 id="amour">Rencontres Amoureuses:</h1>
-<font size="5"  ><p>Naviguez à travers nos profils soigneusement vérifiés et authentifiés, où chaque personne est à la recherche de la même chose que vous : une connexion authentique et profonde. Notre algorithme de compatibilité intelligent vous aide à trouver des partenaires potentiels qui partagent vos valeurs, vos intérêts et vos aspirations.</p>
-</font>
-<center>
-<h2>Nos valeurs:</h2>
-<div class="pic">
-<img src="confid.avif" width=350 height=200>
-<div class="ami">
-<p>Confidentialité</p>
-</div>
-</div>
-<div class="pic">
-<img src="resp.png"width=350 height=200>
-<div class="ami">
-<p>Respect</p>
-</div>
-</div>
-<br>
-<div class="pic">
-<img src="inclu.png" width=350 height=200>
-<div class="ami">
-<p>Inclusivité</p>
-</div>
-</div>
-<div class="pic">
-<img src="love.jpg" width=350 height=200>
-<div class="ami">
-<p>Et Beaucoup d'Amour <<333</p>
-</center>
-</div>
-</div>
-<div class="abonn">
-<h1>Offres D'abonnement</h1>
-<h2>Gratos</h2>
-
-<p>Gratuit</p>
-</ul>
-<h2>Premium</h2>
-<p>Code Abonnement:lhoub2024</p>
-<p>2.99$/mois</p>
-</div>
-<center>
-<p>Rejoignez-nous dès aujourd'hui et commencez votre voyage vers le bonheur! <a href="inscription.php">inscription</a></p>
-</center>
-</div>
-<footer>
-<div class="footer">
-<div class="inline">
-<ul>
-<li ><a href="index.php">Accueil</a></li>
-<li><a href="amicale.php#ami">Rencontres amicales</a></li>
-<li><a href="amicale.php#amour">Rencontres amoureuses</a></li>
-<li><a href="connexion.php">Se connecter</a></li>
-<li><a href="inscription.php">S'inscrire</a><li>
-</ul>
-</div>
-<div class="bottom">
-<p>Copyright Site Web de Rencontre Lhoub 2024; Réalisé par: Imane Safe, Marco Vanni, Arnaud Regnier</p>
-</div>
-</div>
-</div>
-</footer>
-</body>
+	<body>	
+		<div class="head">	
+		<img src="Rimberio.png"style="width:7%">
+		<center>
+			<nav>
+				<div class="table">
+					<ul>
+						<li class="acceuil"><a href="index.php">Accueil</a></li>
+						<li class="amicale"><a href="amicale.php">A propos</a></li>
+						<li class="connexion"><a href="connexion.php">Se connecter</a></li>
+					</ul>
+				</div>
+			</nav>
+		</center>
+		<div class="marg">
+			<font size="6" color="#FF9967">
+				<p id="ami">Notre plateforme offre une manière simple et sécurisée de trouver des amis ou un partenaire à proximité ou partout dans le monde!</p>
+			</font>
+			<h1>Rencontres Amicales:</h1>
+			<font size="5"  >
+				<p>Une fois que vous avez trouvé des profils qui vous intéressent, engagez la conversation en toute simplicité grâce à notre messagerie privée intégrée. Organisez des sorties, des activités ou simplement discutez en ligne, selon vos envies et disponibilités.</p>
+			</font>
+			<center>
+				<div class="pic">
+					<img src="night.jpg" width=350 height=200>
+					<div class="ami">
+						<p>Night Life</p>
+					</div>
+				</div>
+				<div class="pic">
+					<img src="socialnet.jpg"width=350 height=200>
+					<div class="ami">
+						<p>Social Networking</p>
+					</div>
+				</div>
+				<div class="pic">
+					<img src="dining.jpg" width=350 height=200>
+					<div class="ami">
+						<p>Dinner Entre Potes</p>
+					</div>
+				</div>
+				<p>Et bien plus!!</p>
+			</center>
+			<h1 id="amour">Rencontres Amoureuses:</h1>
+			<font size="5">
+				<p>Naviguez à travers nos profils soigneusement vérifiés et authentifiés, où chaque personne est à la recherche de la même chose que vous : une connexion authentique et profonde. Notre algorithme de compatibilité intelligent vous aide à trouver des partenaires potentiels qui partagent vos valeurs, vos intérêts et vos aspirations.</p>
+			</font>
+			<center>
+				<h2>Nos valeurs:</h2>
+				<div class="pic">
+					<img src="confid.avif" width=350 height=200>
+					<div class="ami">
+						<p>Confidentialité</p>
+					</div>
+				</div>
+				<div class="pic">
+					<img src="resp.png"width=350 height=200>
+					<div class="ami">
+						<p>Respect</p>
+					</div>
+				</div>
+				<br>
+				<div class="pic">
+					<img src="inclu.png" width=350 height=200>
+					<div class="ami">
+						<p>Inclusivité</p>
+					</div>
+				</div>
+				<div class="pic">
+					<img src="love.jpg" width=350 height=200>
+					<div class="ami">
+						<p>Et Beaucoup d'Amour <<333</p>
+					</div>
+				</div>
+			</center>
+				<div class="abonn">
+					<h1>Offres D'abonnement</h1>
+					<h2>Gratos</h2>
+					<p>Gratuit</p>
+					<h2>Premium</h2>
+					<p>Code Abonnement:lhoub2024</p>
+					<p>2.99$/mois</p>
+				</div>
+				<center>
+					<p>Rejoignez-nous dès aujourd'hui et commencez votre voyage vers le bonheur! <a href="inscription.php">inscription</a></p>
+				</center>
+		</div>
+		<footer>
+			<div class="footer">
+				<div class="inline">
+					<ul>
+						<li ><a href="index.php">Accueil</a></li>
+						<li><a href="amicale.php#ami">Rencontres amicales</a></li>
+						<li><a href="amicale.php#amour">Rencontres amoureuses</a></li>
+						<li><a href="connexion.php">Se connecter</a></li>
+						<li><a href="inscription.php">S'inscrire</a><li>
+					</ul>
+				</div>
+				<div class="bottom">
+					<p>Copyright Site Web de Rencontre Lhoub 2024; Réalisé par: Imane Safe, Marco Vanni, Arnaud Regnier</p>
+				</div>
+			</div>
+		</footer>
+	</body>
 </html>
